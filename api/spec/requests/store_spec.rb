179df01a44270c2515e398ad3512ef7c63100bd6
Yudai Nakajima <kituneudon97@gmail.com>
2021-06-29 08:52:46 +0000

    #2 余計なコードを削除

--- api/spec/requests/store_spec.rb
+++ api/spec/requests/store_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Stores", type: :request do
-  describe "index" do
-    it "test" do
-      get '/api/v1/stores'
-      assert_response_schema_confirm(200)
-    end
-  end
-
   describe "/stores/:keyword" do
     it "keyword test" do
       get '/api/v1/stores/aaa'
