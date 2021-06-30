1b43481ec31eb0a939669f21a8206e9f88c86083
Yudai Nakajima <kituneudon97@gmail.com>
2021-06-30 02:49:10 +0000

    #6 store_idが入力されていないことを知らせるメッセージを追加

--- api/app/controllers/api/v1/employees_controller.rb
+++ api/app/controllers/api/v1/employees_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::EmployeesController < ApplicationController
   def search
-    employees = Employee.search(params[:store_id])
+    store_id = params[:store_id]
+    unless store_id
+      return render json: { message: "The store_id is not entered." }
+    end
+
+    employees = Employee.search(store_id)
+
     if employees && employees.size > 0
       render json: {
         id: params[:store_id],
