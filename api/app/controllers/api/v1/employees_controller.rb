3bd3d29fe574db34be7cc574e4fae01070572438
Yudai Nakajima <kituneudon97@gmail.com>
2021-06-30 02:58:36 +0000

    #6 エラーメッセージを修正

--- api/app/controllers/api/v1/employees_controller.rb
+++ api/app/controllers/api/v1/employees_controller.rb
@@ -2,18 +2,21 @@ class Api::V1::EmployeesController < ApplicationController
   def search
     store_id = params[:store_id]
     unless store_id
-      return render json: { message: "The store_id is not entered." }
+      return render status: 400, json: { message: "The store_id is not entered." }
     end
 
     employees = Employee.search(store_id)
 
     if employees && employees.size > 0
       render json: {
-        id: params[:store_id],
+        store_id: store_id,
         employees: employees
       }
     else
-      render json: {message: "no data"}
+      render status: 200, json: {
+        store_id: store_id,
+        message: "No data"
+      }
     end
   end
 end
