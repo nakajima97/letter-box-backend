0b9037a5577462dbc246e2e10d4ae2424a1bbd3a
Yudai Nakajima <kituneudon97@gmail.com>
2021-07-02 05:00:45 +0000

    #11 テスト時にDBにデータを入れてから検証するようにした

--- api/app/controllers/api/v1/stores_controller.rb
+++ api/app/controllers/api/v1/stores_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::StoresController < ApplicationController
   def search
-    stores = Store.search(params[:keyword])
-    if stores
-      render json: {data: stores}
+    keyword = params[:keyword]
+    unless keyword
+      return render status: 400, json: {message: "The keyword is not entered."}
+    end
+    stores = Store.search(keyword)
+
+    if stores && stores.size > 0
+      render json: { data: stores }
     else
-      render status: 400, json: {message: "no data"}
+      render json: { message: "No data" }
     end
   end
 end
