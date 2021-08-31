cbe1cd88b7bde8cf63260bbc57bb20cf819cb56a
Yudai Nakajima <kituneudon97@gmail.com>
2021-08-31 07:52:42 +0000

    #26 Gemのアップデート&動かなかったテストを動作するよう変更

--- api/spec/requests/api/v1/messages_spec.rb
+++ api/spec/requests/api/v1/messages_spec.rb
@@ -16,10 +16,7 @@ RSpec.describe "Api::V1::Messages", type: :request do
   describe "create messge" do
     it "success" do
       post '/api/v1/messages', params: { store_id: 1, employee_id:1, message_text: "test" }
-
-      # 400が返ってくる理由が不明
-      # Committee::InvalidResponse: Expected `201` status code, but it was `400`.
-      # assert_response_schema_confirm(201)
+      assert_response_schema_confirm(201)
     end
 
     it "failure" do
