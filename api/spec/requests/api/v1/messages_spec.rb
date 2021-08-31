97068e5755f952b4dcb0a0b8a08c083cbdc1bede
Yudai Nakajima <kituneudon97@gmail.com>
2021-08-31 08:22:56 +0000

    #26 itを日本語に変更

--- api/spec/requests/api/v1/messages_spec.rb
+++ api/spec/requests/api/v1/messages_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Messages", type: :request do
   describe "GET /api/v1/messages" do
-    it "メッセージ一覧を取得" do
+    it "メッセージ一覧を取得する" do
       get '/api/v1/messages'
       assert_response_schema_confirm(200)
     end
 
-    it "従業員IDを指定してメッセージ一覧を取得" do
+    it "従業員IDを指定してメッセージ一覧を取得する" do
       get '/api/v1/messages?employee_id=1'
       assert_response_schema_confirm(200)
     end
   end
 
   describe "POST /api/v1/messages" do
-    it "success" do
+    it "メッセージの投稿に成功する" do
       post '/api/v1/messages', params: { store_id: 1, employee_id:1, message_text: "test" }
       assert_response_schema_confirm(201)
     end
 
-    it "failure" do
+    it "メッセージの投稿に失敗する" do
       post '/api/v1/messages', params: { employee_id:1, message_text: "test" }
       assert_response_schema_confirm(400)
     end
--- api/spec/requests/api/v1/store_spec.rb
+++ api/spec/requests/api/v1/store_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Stores", type: :request do
   describe "GET /api/v1/stores" do
-    it "normal" do
+    it "店舗一覧を取得する" do
       get '/api/v1/stores'
       assert_response_schema_confirm(200)
       res = JSON.parse(response.body)
       expect(res['data']).to eq([{"id"=>1, "name"=>"サンプル株式会社"}, {"id"=>2, "name"=>"株式会社初期値"}])
     end
 
-    it "count set" do
+    it "店舗一覧を取得する際に上限を設定する" do
       get '/api/v1/stores?count=1'
       assert_response_schema_confirm(200)
       res = JSON.parse(response.body)
@@ -19,23 +19,21 @@ RSpec.describe "Stores", type: :request do
   
 
   describe "GET /api/v1/stores/:keyword" do
-    fixtures :stores
-
-    it "success" do
+    it "店舗名に指定したキーワードが含まれている店舗だけを取得する" do
       get '/api/v1/stores/search?keyword=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB'
       assert_response_schema_confirm(200)
       res = JSON.parse(response.body)
       expect(res['data']).to eq([{"id"=>1, "name"=>"サンプル株式会社"}])
     end
 
-    it "no keyword" do
+    it "キーワードを指定せずにキーワード検索を試みる" do
       get '/api/v1/stores/search'
       assert_response_schema_confirm(400)
       res = JSON.parse(response.body)
       expect(res['message']).to eq("The keyword is not entered.")
     end
 
-    it "no data" do
+    it "検索したが、0件だった" do
       get '/api/v1/stores/search?keyword=aaa'
       assert_response_schema_confirm(200)
       res = JSON.parse(response.body)
