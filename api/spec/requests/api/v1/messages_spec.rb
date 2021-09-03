e1987a40910ec2ec4897402fa84b7b09cad5ffab
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-03 05:46:55 +0000

    #21 変更理由をコメントで追加

--- api/spec/requests/api/v1/messages_spec.rb
+++ api/spec/requests/api/v1/messages_spec.rb
@@ -18,6 +18,8 @@ RSpec.describe "Api::V1::Messages", type: :request do
   describe "POST /api/v1/messages" do
     it "メッセージの投稿に成功する" do
       post '/api/v1/messages', params: { store_id: 1, employee_id:1, message_text: "test" }
+      # github actionsでrspecを動かすとここで400が返ってきてrspecが失敗する
+      # 本当は201が返ってくる
       assert_response_schema_confirm
     end
 
