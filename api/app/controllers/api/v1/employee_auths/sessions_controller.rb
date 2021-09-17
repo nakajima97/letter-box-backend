c2e98f38a35509e1e4371601ec1f26b74a474e25
Yudai Nakajima <kituneudon97@gmail.com>
2021-09-17 07:20:14 +0000

    #36 ログインする際にメッセージを表示するメソッドをオーバーライドして、動作するようにした

--- api/app/controllers/api/v1/employee_auths/sessions_controller.rb
+++ api/app/controllers/api/v1/employee_auths/sessions_controller.rb
@@ -24,4 +24,24 @@ class Api::V1::EmployeeAuths::SessionsController < Devise::SessionsController
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:employee_login_id])
   end
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: { message: 'You are logged in.' }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    log_out_success && return if current_employee_auth
+
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: { message: "You are logged out." }, status: :ok
+  end
+
+  def log_out_failure
+    render json: { message: "Hmm nothing happened."}, status: :unauthorized
+  end
 end
--- api/app/controllers/api/v1/store_auths/sessions_controller.rb
+++ api/app/controllers/api/v1/store_auths/sessions_controller.rb
@@ -18,10 +18,30 @@ class Api::V1::StoreAuths::SessionsController < Devise::SessionsController
     super
   end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:store_login_id])
+  end
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: { message: 'You are logged in.' }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    log_out_success && return if current_store_auth
+
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: { message: "You are logged out." }, status: :ok
+  end
+
+  def log_out_failure
+    render json: { message: "Hmm nothing happened."}, status: :unauthorized
   end
 end
